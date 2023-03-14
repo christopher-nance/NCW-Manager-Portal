045f027df1c1c1c0ab781a3d30ab084dd85873bc
Chris Nance <christopher.nance@icloud.com>
2023-03-14 13:30:33 -0500

    Added login & authentication.

--- NCW_Manager_Portal/views.py
+++ NCW_Manager_Portal/views.py
@@ -3,13 +3,52 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+import flask
+from flask import render_template, session, request, redirect, url_for
 from NCW_Manager_Portal import app
+import msal
+
+app = flask(__name__)
+app.secret_key = "my_secret_key"
+
+# Configure MSAL settings
+CLIENT_ID = "e732516b-f803-473a-bb61-bcd394488ee1"
+CLIENT_SECRET = "_Xv8Q~bsNq0hGydp3QUJJMSLuK~GTukZ3HGHJc-j"
+AUTHORITY = "https://login.microsoftonline.com/16abb6e6-23e2-421c-8afd-0e7a19ce0fe0"
+REDIRECT_URI = "https://manager-portal-rg.azurewebsites.net/callback"
+SCOPE = ["User.Read"]
+
+# Create a new MSAL client
+msal_client = msal.ConfidentialClientApplication(CLIENT_ID, client_credential=CLIENT_SECRET, authority=AUTHORITY)
+
+# Define the login route
+@app.route("/login")
+def login():
+    # Redirect the user to the Azure AD login page
+    auth_url = msal_client.get_authorization_request_url(SCOPE, redirect_uri=REDIRECT_URI)
+    return redirect(auth_url)
+
+# Define the callback route
+@app.route("/callback")
+def callback():
+    # Handle the authentication response from Azure AD
+    if "code" in request.args:
+        result = msal_client.acquire_token_by_authorization_code(request.args["code"], SCOPE, redirect_uri=REDIRECT_URI)
+        if "access_token" in result:
+            session["access_token"] = result["access_token"]
+            return redirect(url_for("home"))
+    return "Authentication failed."
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
+    if "access_token" not in session:
+        return redirect(url_for("login"))
+    # Use the access token to verify the user's identity and authorize access to protected resources
+    headers = {"Authorization": "Bearer " + session["access_token"]}
+    response = requests.get("https://graph.microsoft.com/v1.0/me", headers=headers)
     return render_template(
         'index.html',
         title='Home Page',
